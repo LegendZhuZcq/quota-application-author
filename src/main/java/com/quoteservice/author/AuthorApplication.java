774fb92cce16aa803c0a20d0c3233f7be4384e50
LegendZhuZcq <zhu.legend@gmail.com>
2017-10-13 15:29:09 -0400

    finished hw3

--- src/main/java/com/quoteservice/author/AuthorApplication.java
+++ src/main/java/com/quoteservice/author/AuthorApplication.java
@@ -1,16 +1,14 @@
 package com.quoteservice.author;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthorApplication {
 
-	@Autowired
-	private AuthorData authorData;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorApplication.class, args);
 	}
--- src/main/java/com/quoteservice/author/controller/AuthorController.java
+++ src/main/java/com/quoteservice/author/controller/AuthorController.java
@@ -1,6 +1,11 @@
 package com.quoteservice.author.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
